37455ac26f56908f64ae96e77e0fec19db0e68d7
Shlomi Matichin <shlomomatichin@gmail.com>
2018-06-15 21:32:30 +0300

    first version

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,90 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+    onDeviceReady: function() {}
 };
 
 app.initialize();
+
+var counter1;
+var counter2;
+var startedAt;
+var finished;
+var timerLength = 35000;
+var timerHandle;
+function reset() {
+    counter1 = 0;
+    counter2 = 0;
+    startedAt = undefined;
+    finished = false;
+}
+reset();
+
+var counter1ValueElement = $("#counter1Value");
+var counter2ValueElement = $("#counter2Value");
+var timeElement = $("#time");
+function updateDisplay() {
+    counter1ValueElement.html("" + counter1);
+    counter2ValueElement.html("" + counter2);
+    if (startedAt === undefined)
+        timeElement.html("00:000");
+    else {
+        var now = new Date();
+        var delta = now.getTime() - startedAt.getTime();
+        if (delta > timerLength) {
+            delta = timerLength;
+            finished = true;
+        }
+        var seconds = Math.floor(delta / 1000)
+        var millis = ("00" + (delta % 1000));
+        millis = millis.substr(millis.length - 3);
+        timeElement.html("" + seconds + ":" + millis);
+    }
+}
+
+function start() {
+    startedAt = new Date();
+    updateDisplay();
+    armTimer();
+}
+function armTimer() {
+    timerHandle = setTimeout(function() {
+        updateDisplay();
+        if (finished)
+            return;
+        armTimer();
+    }, 1000 / 60);
+}
+
+$("#counter1Button").click(function() {
+    if (finished)
+        return;
+    if (startedAt === undefined)
+        start();
+    else {
+        counter1 += 1;
+        updateDisplay();
+    }
+});
+$("#counter2Button").click(function() {
+    if (finished)
+        return;
+    if (startedAt === undefined)
+        start();
+    else {
+        counter2 += 1;
+        updateDisplay();
+    }
+});
+$("#counter1Button").mousedown(function() { $(this).addClass("pressed"); });
+$("#counter1Button").mouseup(function() { $(this).removeClass("pressed"); });
+$("#counter2Button").mousedown(function() { $(this).addClass("pressed"); });
+$("#counter2Button").mouseup(function() { $(this).removeClass("pressed"); });
+
+$("#resetButton").click(function() {
+    clearTimeout(timerHandle);
+    reset();
+    updateDisplay();
+});
